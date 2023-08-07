e1735d0e4b1289353a30d5ee055537f621a25634
dhairya_mehta <ldhairya2000@gmail.com>
2023-08-07 23:48:00 +0530

    Shimmer Changes done

--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -1,48 +1,63 @@
 const Shimmer = () => {
   return (
     <>
-    <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
-      <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
-      <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
-      <div className="bg-gray-300 h-4 rounded-lg"></div>
-    </div>
-    <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
-      <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
-      <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
-      <div className="bg-gray-300 h-4 rounded-lg"></div>
-    </div>
-    <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
-      <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
-      <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
-      <div className="bg-gray-300 h-4 rounded-lg"></div>
-    </div>
-    <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
-      <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
-      <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
-      <div className="bg-gray-300 h-4 rounded-lg"></div>
-    </div>
-    <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
-      <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
-      <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
-      <div className="bg-gray-300 h-4 rounded-lg"></div>
-    </div>
-    <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
-      <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
-      <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
-      <div className="bg-gray-300 h-4 rounded-lg"></div>
-    </div>
-    <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
-      <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
-      <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
-      <div className="bg-gray-300 h-4 rounded-lg"></div>
-    </div>
-    <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
-      <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
-      <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
-      <div className="bg-gray-300 h-4 rounded-lg"></div>
-    </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
+          <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
+          <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
+          <div className="bg-gray-300 h-4 rounded-lg"></div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
+          <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
+          <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
+          <div className="bg-gray-300 h-4 rounded-lg"></div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
+          <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
+          <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
+          <div className="bg-gray-300 h-4 rounded-lg"></div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
+          <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
+          <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
+          <div className="bg-gray-300 h-4 rounded-lg"></div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
+          <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
+          <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
+          <div className="bg-gray-300 h-4 rounded-lg"></div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
+          <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
+          <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
+          <div className="bg-gray-300 h-4 rounded-lg"></div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
+          <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
+          <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
+          <div className="bg-gray-300 h-4 rounded-lg"></div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-64 h-72 bg-white p-4 rounded-lg shadow-md animate-pulse">
+          <div className="bg-gray-300 h-40 rounded-lg mb-4"></div>
+          <div className="bg-gray-300 h-6 rounded-lg mb-2"></div>
+          <div className="bg-gray-300 h-4 rounded-lg"></div>
+        </div>
+      </div>
     </>
-    
   );
 };
 
